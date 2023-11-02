1e999812259b3571d7517707d6b23796feba9ac4
Ashley Trinh <itsashley@hey.com>
2023-11-02 12:04:53 -0700

    chore: migrate directives to sphinx-bootcampstrap

--- src/sphinx_handouts/directives/__init__.py
+++ src/sphinx_handouts/directives/__init__.py
@@ -1,12 +1,10 @@
 from typing import TYPE_CHECKING
 
-from . import compare, console, admonitions
+from . import admonitions
 
 if TYPE_CHECKING:
     from sphinx.application import Sphinx
 
 
 def setup(app: "Sphinx") -> None:
-    compare.setup(app)
-    console.setup(app)
     admonitions.setup(app)
