19807164b77d5064f17686f0459a48577464e1b3
Ashley Trinh <itsashley@hey.com>
2023-11-02 12:05:02 -0700

    chore: update example

--- examples/kitchen-sink/conf.py
+++ examples/kitchen-sink/conf.py
@@ -1,4 +1,4 @@
-extensions = ["sphinx_handouts"]
+extensions = ["sphinx_bootcampstrap", "sphinx_handouts"]
 html_theme = "seibootcamps"
 seibootcamps_html_preconnect = [
     "https://fonts.googleapis.com",
@@ -8,7 +8,6 @@ seibootcamps_html_preconnect = [
 html_static_path = ["_static"]
 html_css_files = [
     "https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Source+Sans+3:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
-    "overrides.css",
 ]
 html_js_files = [("emphasize-lines.js", {"defer": "defer"})]
 
